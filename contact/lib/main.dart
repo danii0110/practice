9360ffcf57cba5051cd602dfb0553b774ac3d32e
danii0110 <105475360+danii0110@users.noreply.github.com>
2023-07-02 17:04:19 +0900

    Flutter에서 박스디자인하는 법(margin, padding, 정렬)

--- contact/lib/main.dart
+++ contact/lib/main.dart
@@ -11,24 +11,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('앱임'),
-          centerTitle: false,
-        ),
-        body: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Text('안녕'),
-          ],
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(icon: Icon(Icons.phone), label: 'phone'),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.message), label: 'message'),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.contact_page), label: 'contact page'),
-          ],
+        appBar: AppBar(title: Text('앱임')),
+        body: Align(
+          alignment: Alignment.topCenter,
+          child: Container(
+            width: double.infinity,
+            height: 50,
+            color: Colors.blue,
+          ),
         ),
       ),
     );
