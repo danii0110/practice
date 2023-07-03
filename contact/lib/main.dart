92816c8669d9688090a75a0c4a6bda0409443321
danii0110 <105475360+danii0110@users.noreply.github.com>
2023-07-03 15:37:16 +0900

    Expanded, Flexible & 상품 레이아웃 숙제

--- contact/lib/main.dart
+++ contact/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,10 +14,56 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          actions: [Icon(Icons.star), Icon(Icons.star)],
-          title: Text('ddd'),
+          title: Text(
+            '금호동3가',
+            style: TextStyle(color: Colors.black),
+          ),
+          centerTitle: false,
+          elevation: 0.0,
+          backgroundColor: Colors.white,
+          actions: [
+            Icon(
+              Icons.search,
+              color: Colors.black,
+            ),
+            Icon(
+              Icons.menu,
+              color: Colors.black,
+            ),
+            Icon(
+              Icons.notifications_none,
+              color: Colors.black,
+            )
+          ],
         ),
-        body: SizedBox(),
+        body: Container(
+          height: 150,
+          padding: EdgeInsets.all(10),
+          child: Row(
+            children: [
+              Image.asset(
+                '/Users/dani/Desktop/dani/practice/contact/assets/images/camera.png',
+                width: 150,
+                height: 150,
+              ),
+              Container(
+                width: 250,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('카메라팝니다.'),
+                    Text('금호동 3가'),
+                    Text('7000원'),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [Icon(Icons.favorite), Text('4')],
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
