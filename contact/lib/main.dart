8921733cc92b0695a63cad7c8dfbe6c181da23b4
danii0110 <105475360+danii0110@users.noreply.github.com>
2023-07-03 15:59:08 +0900

    코드가 길어지면 커스텀 위젯 만드세요

--- contact/lib/main.dart
+++ contact/lib/main.dart
@@ -1,4 +1,4 @@
-import 'dart:io';
+//import 'dart:io';
 
 import 'package:flutter/material.dart';
 
@@ -13,58 +13,26 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            '금호동3가',
-            style: TextStyle(color: Colors.black),
-          ),
-          centerTitle: false,
-          elevation: 0.0,
-          backgroundColor: Colors.white,
-          actions: [
-            Icon(
-              Icons.search,
-              color: Colors.black,
-            ),
-            Icon(
-              Icons.menu,
-              color: Colors.black,
-            ),
-            Icon(
-              Icons.notifications_none,
-              color: Colors.black,
-            )
+        appBar: AppBar(),
+        body: ListView(
+          children: [
+            Account(),
+            Account(),
+            Account(),
           ],
         ),
-        body: Container(
-          height: 150,
-          padding: EdgeInsets.all(10),
-          child: Row(
-            children: [
-              Image.asset(
-                '/Users/dani/Desktop/dani/practice/contact/assets/images/camera.png',
-                width: 150,
-                height: 150,
-              ),
-              Container(
-                width: 250,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('카메라팝니다.'),
-                    Text('금호동 3가'),
-                    Text('7000원'),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [Icon(Icons.favorite), Text('4')],
-                    )
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
       ),
+    );
+  }
+}
+
+class Account extends StatelessWidget {
+  const Account({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [Icon(Icons.account_circle), Text('홍길동')],
     );
   }
 }
